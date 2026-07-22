e4dd3e22b67a589d7740dc83bd08b3e4a91befa4
varshithboorla <varshithboorla@gmail.com>
2026-07-22 14:32:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
@@ -0,0 +1,32 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+class Solution {
+    public ArrayList<Integer> leftView(Node root) {
+        // code here
+        Queue<Node> q = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        q.offer(root);
+        while(!q.isEmpty()){
+            int len = q.size();
+            for(int i=0;i<len;i++){
+                Node temp = q.poll();
+                if(i==0) list.add(temp.data);
+                if(temp.left!=null) q.offer(temp.left);
+                if(temp.right!=null) q.offer(temp.right);
+            }
+        }
+        return list;
+    }
+}
